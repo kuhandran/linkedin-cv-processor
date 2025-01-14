f983d71442772db647fec7e9b9e3591e0728c168
Kuhandran <skuhandran@yahoo.com>
2025-01-14 11:43:34 +0800

    required to update the python files ensure we run smooth services

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ app.include_router(root.router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Load additional routes conditionally
-# load_all_routes = os.getenv("LOAD_ALL_ROUTES", "false").lower() == "true"
+load_all_routes = os.getenv("LOAD_ALL_ROUTES", "false").lower() == "true"
 
-# if load_all_routes:
-#     app.include_router(question.router)
-#     app.include_router(profile.router)
-#     app.include_router(languages.router)
+if load_all_routes:
+    app.include_router(question.router)
+    app.include_router(profile.router)
+    app.include_router(languages.router)
 
 if __name__ == "__main__":
     import uvicorn
